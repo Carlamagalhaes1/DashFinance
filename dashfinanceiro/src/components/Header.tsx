da968b4d2862536c7e109039c28a4b4688f970c7
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-08-13 20:34:02 -0300

    input centralizado no header de trasaçõe

--- dashfinanceiro/src/components/Header.tsx
+++ dashfinanceiro/src/components/Header.tsx
@@ -3,25 +3,26 @@ import { FaUserCircle } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between bg-[#EDEAFF] px-6 py-4">
+    <header className="relative flex items-center justify-between bg-[#EDEAFF] px-6 py-4">
+      {/* Esquerda: Título */}
       <h1 className="font-bold text-lg text-[#2E12E4]">SUAS TRANSAÇÕES</h1>
 
-      <div className="flex items-center gap-4">
-        {/* Pesquisa */}
+      {/* Centro: Pesquisa */}
+      <div className="absolute left-1/2 -translate-x-1/2">
         <div className="relative">
           <input
             type="text"
             placeholder="Pesquisar transação"
-            className="bg-white rounded-full px-4 py-2 pl-10 focus:outline-none"
+            className="bg-white w-80 rounded-full px-4 py-2 pl-10 focus:outline-none"
           />
           <FaSearch className="absolute left-3 top-3 text-gray-400" />
         </div>
+      </div>
 
-        {/* Usuário */}
-        <div className="flex items-center gap-2 text-[#2E12E4]">
-          <FaUserCircle size={28} />
-          <span>Carla</span>
-        </div>
+      {/* Direita: Usuário */}
+      <div className="flex items-center gap-2 text-[#2E12E4]">
+        <FaUserCircle size={28} />
+        <span>Carla</span>
       </div>
     </header>
   );
