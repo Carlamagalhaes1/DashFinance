dc468468f318228118d3853fd6215ca9992b586c
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-08-20 23:56:37 -0300

    Implementa toggle da sidebar: botão seta altera estado colapsada, ajusta largura e renderização condicional do título

--- dashfinanceiro/src/components/Sidebar.tsx
+++ dashfinanceiro/src/components/Sidebar.tsx
@@ -3,37 +3,47 @@ import { FaRegFileAlt } from "react-icons/fa";
 import { IoMdColorPalette } from "react-icons/io";
 import { GiTargetPrize } from "react-icons/gi";
 import { RiDashboardFill } from "react-icons/ri";
+import { BsArrowLeft } from "react-icons/bs";
+import { useState } from "react";
+
 
 export default function Sidebar() {
+
+  const [colapsada, setColapsada]= useState(false)
+
+  
   return (
-    <aside className="bg-[#381CD5] w-56 h-screen flex flex-col justify-between">
+    <aside className={`bg-[#381CD5] ${colapsada ? "w-20" :  "w-56"} h-screen flex flex-col justify-between transition-all duration-300`}>
+      
       <div>
-        {/* Logo */}
+       
         <div className="flex items-center gap-2 px-4 py-6">
-          <img src="images/logo2.svg" alt="RegistraAí" className="w-10 h-10" />
+          {!colapsada && <h1>ASSIANAIA</h1>}
         
         </div>
-
-        {/* Menu */}
+        <div className="flex justify-items-center cursor-pointer" onClick={() => setColapsada(!colapsada)}> {colapsada ? <BsArrowLeft className=" transition-all duration-300"  /> : <BsArrowLeft className="rotate-180 transition-all duration-300"/>} </div>
+        
+        
         <nav className="flex flex-col mt-4">
+          
           <a href="#" className="flex items-center gap-2 px-4 py-2 bg-[#604BEE] text-xl font-bold text-[#0C0073] rounded-r-full">
-            <FaRegFileAlt /> Transações
+            <FaRegFileAlt />{!colapsada && "Transações"}
           </a>
           <a href="#" className="flex items-center gap-2 px-4 py-2 font-bold text-xl text-white hover:bg-[#604BEE] rounded-r-full">
-            <RiDashboardFill /> Dashboard
+            <RiDashboardFill /> {!colapsada && "Dashboard"}
           </a>
           <a href="#" className="flex items-center gap-2 px-4 py-2 font-bold text-xl text-white hover:bg-[#604BEE] rounded-r-full">
-            <IoMdColorPalette /> Tema
+            <IoMdColorPalette /> {!colapsada && "Tema"}
           </a>
           <a href="#" className="flex items-center gap-2 px-4 py-2 font-bold text-xl text-white hover:bg-[#604BEE] rounded-r-full">
-            <GiTargetPrize /> Metas
+            <GiTargetPrize /> {!colapsada && "Metas"}
           </a>
         </nav>
       </div>
 
-      {/* Sair */}
+      
       <div className="flex items-center gap-2 px-4 py-4 text-white cursor-pointer hover:bg-[#604BEE]">
-        <FaSignOutAlt /> Sair
+        <FaSignOutAlt /> {!colapsada && "Sair"}
       </div>
     </aside>
   );
